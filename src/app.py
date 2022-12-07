64ae5a98a4beefec541ea14d99590116cc416b99
w-a-gomes <wildenalisson@outlook.com>
2022-12-07 19:52:23 -0300

    Fix: TypeError: 'int' object is not callable

--- src/app.py
+++ src/app.py
@@ -4,7 +4,6 @@ import sys
 from BlurWindow.blurWindow import GlobalBlur
 from PySide6 import QtCore, QtGui, QtWidgets
 from __feature__ import snake_case
-from __feature__ import true_property
 
 import ui
 
@@ -45,15 +44,15 @@ class TuxMenu(object):
     def main(self) -> None:
         """Start the app."""
         # Name
-        self.app_ui.window_title = self.app_name
+        self.app_ui.set_window_title(self.app_name)
 
         # Icon
         app_icon = QtGui.QIcon(QtGui.QPixmap(self.app_icon))
-        self.app_ui.window_icon = app_icon
+        self.app_ui.set_window_icon(app_icon)
 
         # Size
-        self.app_ui.minimum_height = 600  # .size = QtCore.QSize(300, 500)
-        self.app_ui.minimum_width = 1000  # .show_maximized()
+        self.app_ui.set_minimum_height(600)
+        self.app_ui.set_minimum_width(1000)
 
         # Blur
         self.app_ui.set_attribute(QtCore.Qt.WA_TranslucentBackground)
--- src/ui.py
+++ src/ui.py
@@ -3,7 +3,6 @@ from xdg import IconTheme
 
 from PySide6 import QtCore, QtGui, QtWidgets
 from __feature__ import snake_case
-from __feature__ import true_property
 
 import desktopentryparse
 
@@ -15,21 +14,18 @@ class MainWindow(QtWidgets.QMainWindow):
         super().__init__(*args, **kwargs)
         # Main container
         self.main_container = QtWidgets.QWidget()
-        self.main_container.contents_margins = QtCore.QMargins(0, 0, 0, 0)
-        self.main_container.spacing = 0
+        self.main_container.set_contents_margins(0, 0, 0, 0)
         self.set_central_widget(self.main_container)
 
         # Main layout
         self.layout_container = QtWidgets.QVBoxLayout()
-        self.layout_container.contents_margins = QtCore.QMargins(0, 0, 0, 0)
-        self.layout_container.spacing = 0
+        self.layout_container.set_spacing(0)
         self.main_container.set_layout(self.layout_container)
 
         # App launcher
         self.app_launcher_layout = QtWidgets.QVBoxLayout()
-        self.app_launcher_layout.contents_margins = QtCore.QMargins(0, 0, 0, 0)
-        self.app_launcher_layout.spacing = 0
-        self.app_launcher_layout.alignment = QtCore.Qt.AlignCenter
+        self.app_launcher_layout.set_spacing(0)
+        self.app_launcher_layout.set_alignment(QtCore.Qt.AlignCenter)
         self.layout_container.add_layout(self.app_launcher_layout)
 
         self.app_launcher = AppLauncher(
@@ -43,17 +39,14 @@ class AppLauncher(QtWidgets.QWidget):
     def __init__(self, desktop_file, *args, **kwargs):
         """Class constructor."""
         super().__init__(*args, **kwargs)
-        # Main container
         self.layout_container = QtWidgets.QVBoxLayout()
-        self.layout_container.contents_margins = QtCore.QMargins(0, 0, 0, 0)
-        self.layout_container.spacing = 0
+        self.layout_container.set_spacing(0)
         self.set_layout(self.layout_container)
 
         # Icon
         self.icon_layout = QtWidgets.QVBoxLayout()
-        self.icon_layout.contents_margins = QtCore.QMargins(0, 0, 0, 0)
-        self.icon_layout.spacing = 0
-        self.icon_layout.alignment = QtCore.Qt.AlignCenter
+        self.icon_layout.set_spacing(0)
+        self.icon_layout.set_alignment(QtCore.Qt.AlignCenter)
         self.layout_container.add_layout(self.icon_layout)
 
         self.icon_path = IconTheme.getIconPath(
@@ -67,8 +60,8 @@ class AppLauncher(QtWidgets.QWidget):
             48, 48, QtCore.Qt.KeepAspectRatio)
 
         self.icon_label = QtWidgets.QLabel(self)
-        self.icon_label.stylesheet = (
+        self.icon_label.set_style_sheet(
             'background-color: transparent; padding-top: 10;')
-        self.icon_label.pixmap = self.scaled_pixmap
-        self.icon_label.alignment = QtCore.Qt.AlignCenter
+        self.icon_label.set_pixmap(self.scaled_pixmap)
+        self.icon_label.set_alignment(QtCore.Qt.AlignCenter)
         self.icon_layout.add_widget(self.icon_label)
